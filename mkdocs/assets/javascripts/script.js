43b4b6e7282a7c158f24699c7de561a500bdde79
Fabiano <fabianotmp@gmail.com>
2025-05-21 14:31:24 -0300

    build

--- mkdocs/assets/javascripts/script.js
+++
@@ -1,6 +0,0 @@
-const botoes = document.querySelectorAll("button[type='submit']");
-botoes.forEach(function(btn) {
-  if (btn.textContent.trim() === "Submit") {
-    btn.textContent = "Enviar";
-  }
-});
